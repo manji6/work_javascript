1a8b1eae33d5e4374278dbaa70a9ec0ca58b5cad
unknown <ry-sawada@RY-SAWADA2.opendoor.local>
2012-03-07 17:22:36 +0900

    fix encode

--- public_html/jquery_mobile/datebox/jquery.mobile.datebox.action.js
+++ public_html/jquery_mobile/datebox/jquery.mobile.datebox.action.js
@@ -1,4 +1,16 @@
+// jQuery mobile calbox default setting
+jQuery.extend(jQuery.mobile.datebox.prototype.options, {
+	'dateFormat': 'YYYY年 mm月 dd日',
+	'headerFormat': 'YYYY年 mm月 dd日'
+});
+
 $(function(){
 
-
+	$('#mydate').bind('datebox', function (e, passed) {
+		if ( passed.method === 'set' ) {
+			e.stopImmediatePropagation();
+			//DO SOMETHING//
+			console.log("setしますた");
+		}
+	});
 });
